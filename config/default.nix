fefb8503edca0224c56b5938c3a43a7f0bc5dbd4
theeasternfurry <theeasternfurry@outlook.com>
2026-01-30 08:25:15 +0700

    feat: add lsp, lualine and treesitter

--- config/default.nix
+++ config/default.nix
@@ -2,6 +2,9 @@
   # Import all your configuration modules here
   imports = [
     ./bufferline.nix
+    ./lsp.nix
+    ./lualine.nix
+    ./treesitter.nix
     ./yazi.nix
   ];
 }
---
+++ config/lsp.nix
@@ -0,0 +1,16 @@
+{
+  plugins.lsp = {
+    enable = true;
+    servers = {
+      docker_language_server.enable = true;
+      nil_ls.enable = true;
+      rust_analyzer = {
+        enable = true;
+        installCargo = true;
+        installRustc = true;
+      };
+      svelte.enable = true;
+      ts_ls.enable = true;
+    };
+  };
+}
