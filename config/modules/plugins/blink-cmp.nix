66b8f2b42e2dfc362319bd0379b7cdc1947e7217
theeasternfurry <theeasternfurry@outlook.com>
2026-02-13 16:07:18 +0700

    feat: blink add key map accept, previous & next

--- config/modules/plugins/blink-cmp.nix
+++ config/modules/plugins/blink-cmp.nix
@@ -3,7 +3,16 @@
     enable = true;
     settings = {
       keymap = {
-        preset = "default"; 
+        "<C-space>" = [ "show" "show_documentation" "hide_documentation" ];
+        "<C-e>" = [ "hide" ];
+        "<C-y>" = [ "select_and_accept" ];
+        "<C-p>" = [ "select_prev" "fallback" ];
+        "<C-n>" = [ "select_next" "fallback" ];
+        "<C-b>" = [ "scroll_documentation_up" "fallback" ];
+        "<C-f>" = [ "scroll_documentation_down" "fallback" ];
+        "<CR>" = [ "accept" ];
+        "<Tab>" = [ "select_next" "fallback" ];
+        "<S-Tab>" = [ "select_prev" "fallback" ];
       };
 
       sources = {
@@ -13,6 +22,6 @@
       appearance = {
         nerd_font_variant = "mono";
       };
-  };
+    };
   };
 }
