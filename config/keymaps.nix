2e0fc870f3b32813529f15ea43b459d6c5d2c100
theeasternfurry <theeasternfurry@outlook.com>
2026-02-13 15:59:17 +0700

    feat: add keymaps indent code left & right

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -8,5 +8,17 @@
         desc = "Open Yazi (File Manager)";
       };
     }
+    {
+      mode = ["x" "v"];
+      key = "<Tab>";
+      action = ">gv";
+      options.desc = "Indent right";
+    }
+    {
+      mode = ["x" "v"];
+      key = "<S-Tab>";
+      action = "<gv"; 
+      options.desc = "Indent left";
+    }
   ];
 }
