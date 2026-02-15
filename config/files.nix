a399a0df634c48318dfb7089a7a7f757b4d93c69
theeasternfurry <theeasternfurry@outlook.com>
2026-02-15 21:27:16 +0700

    feat: update more files type

--- config/files.nix
+++ config/files.nix
@@ -1,39 +1,31 @@
 {
-  files = {
-    "ftplugin/python.lua" = {
-      opts = {
-        expandtab = true;
-        shiftwidth = 4;
-        tabstop = 4;
-        softtabstop = 4;
-      };
+  files = let
+    tab2spaces = {
+      expandtab = true;
+      shiftwidth = 2;
+      tabstop = 2;
+      softtabstop = 2;
     };
-
-    "ftplugin/rust.lua" = {
-      opts = {
-        expandtab = true;
-        shiftwidth = 4;
-        tabstop = 4;
-        softtabstop = 4;
-      };
+    
+    tab4spaces = {
+      expandtab = true;
+      shiftwidth = 4;
+      tabstop = 4;
+      softtabstop = 4;
     };
-
-    "ftplugin/nix.lua" = {
-      opts = {
-        expandtab = true;
-        shiftwidth = 2;
-        tabstop = 2;
-        softtabstop = 2;
-      };
-    };
-
-    "ftplugin/svelte.lua" = {
-      opts = {
-        expandtab = true;
-        shiftwidth = 2;
-        tabstop = 2;
-        softtabstop = 2;
-      };
-    };
+  in {
+    "ftplugin/python.lua" = { opts = tab4spaces; };
+    "ftplugin/rust.lua" = { opts = tab4spaces; };
+    "ftplugin/nix.lua" = { opts = tab2spaces; };
+    "ftplugin/svelte.lua" = { opts = tab2spaces; };
+    "ftplugin/typescript.lua" = { opts = tab2spaces; };
+    "ftplugin/typescriptreact.lua" = { opts = tab2spaces; };
+    "ftplugin/javascript.lua" = { opts = tab2spaces; };
+    "ftplugin/javascriptreact.lua" = { opts = tab2spaces; };
+    "ftplugin/json.lua" = { opts = tab2spaces; };
+    "ftplugin/jsonc.lua" = { opts = tab2spaces; };
+    "ftplugin/html.lua" = { opts = tab2spaces; };
+    "ftplugin/css.lua" = { opts = tab2spaces; };
+    "ftplugin/scss.lua" = { opts = tab2spaces; };
   };
 }
