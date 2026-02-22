c90010d72193eb5b3556aa47338b09a02f13ca5b
theeasternfurry <theeasternfurry@outlook.com>
2026-02-22 21:49:52 +0700

    fix: trouble add keymaps and settings

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -20,5 +20,53 @@
       action = "<gv"; 
       options.desc = "Indent left";
     }
+    {
+      mode = "n";
+      key = "<leader>xx";
+      action = "<cmd>Trouble diagnostics toggle<cr>";
+      options = {
+        desc = "Diagnostics (Trouble)";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>xX";
+      action = "<cmd>Trouble diagnostics toggle filter.buf=0<cr>";
+      options = {
+        desc = "Buffer Diagnostics (Trouble)";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>cs";
+      action = "<cmd>Trouble symbols toggle<cr>";
+      options = {
+        desc = "Symbols (Trouble)";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>cl";
+      action = "<cmd>Trouble lsp toggle<cr>";
+      options = {
+        desc = "LSP Definitions / references (Trouble)";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>xL";
+      action = "<cmd>Trouble loclist toggle<cr>";
+      options = {
+        desc = "Location List (Trouble)";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>xQ";
+      action = "<cmd>Trouble qflist toggle<cr>";
+      options = {
+        desc = "Quickfix List (Trouble)";
+      };
+    }
   ];
 }
--- config/modules/plugins/trouble.nix
+++ config/modules/plugins/trouble.nix
@@ -1,3 +1,18 @@
 {
-  plugins.trouble.enable = true;
+  plugins.trouble = {
+    enable = true;
+
+    settings = {
+      autoOpen = false;
+      autoClose = true;
+      autoPreview = true;
+      focus = false;
+      restore = true;
+      follow = true;
+      indentLines = true;
+      win = {
+        size = 10;  # Kích thước cửa sổ
+      };
+    };
+  };
 }
